87ff769f576b13f019e2726d3fbd637c8c1e1776
dwkim <kdw2139@gmail.com>
2017-11-15 22:42:34 +0900

    error fix on metric to load

--- load.py
+++ load.py
@@ -5,10 +5,14 @@ import extract_data as ed
 from keras.models import model_from_json
 import graph
 import keras.backend as K
+import numpy as np
+
 
 # Define metric
 def mean_acc(y_true, y_pred):
-    return K.mean(1-K.abs(y_true-y_pred)/y_true)
+    if y_true == 0:
+        return 0
+    return K.mean(1-K.abs(y_true-y_pred)/(y_true))
 
 model_file = (sys.argv[1]).split('.')[0]
 data_file = sys.argv[2]
@@ -40,9 +44,12 @@ print("Loaded model from disk")
 # evaluate loaded model on test data
 # loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 loaded_model.compile(loss='mean_squared_error', optimizer='adam', metrics=['mean_squared_error', mean_acc])
-score = loaded_model.evaluate(x_data, y_data, verbose=0)
+score = loaded_model.evaluate(x_data, y_data, verbose=1)
 print "ACC: " + str(score[2] * 100) + "%"
 print "MSE: " + str(score[1])
 
 est = loaded_model.predict(x_data)
 graph.draw_graph(x_data[:, -1], y_data, est)
+
+acc = (1-((np.abs(y_data - est)/est).mean()))*100
+print "ACC-: " + str(acc) + "%"
